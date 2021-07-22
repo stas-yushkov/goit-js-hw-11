c6ef1121b36211875490ca1b0df26fa94569f3fb
stas-yushkov <77212175+stas-yushkov@users.noreply.github.com>
2021-07-22 13:45:06 +0000

    Deploying to gh-pages from @ stas-yushkov/goit-js-hw-11@eea27de0d63ab8514d13a6abe7f29065030b26a4 🚀

--- src.540184bd.js
+++ src.540184bd.js
@@ -1,8 +1,6 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"Y5dV":[function(require,module,exports) {
 
-},{}],"clu1":[function(require,module,exports) {
-
-},{}],"TCaP":[function(require,module,exports) {
-"use strict";require("modern-normalize/modern-normalize.css"),require("../sass/main.scss");const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let o;function n(n){if(n.target===t){o=setInterval(()=>{r.style=`background-color: ${a()};`},1e3),t.setAttribute("disabled",!0)}n.target===e&&(clearInterval(o),t.removeAttribute("disabled"))}function a(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}r.addEventListener("click",n);
-},{"modern-normalize/modern-normalize.css":"Y5dV","../sass/main.scss":"clu1"}]},{},["TCaP"], null)
-//# sourceMappingURL=/goit-js-hw-11/01-color-switcher.44264442.js.map
+},{}],"Focm":[function(require,module,exports) {
+"use strict";require("modern-normalize/modern-normalize"),require("./sass/main.scss");
+},{"modern-normalize/modern-normalize":"Y5dV","./sass/main.scss":"Y5dV"}]},{},["Focm"], null)
+//# sourceMappingURL=/parcel-project-template/src.540184bd.js.map
